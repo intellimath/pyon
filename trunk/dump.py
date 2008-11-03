874a5cbcd015dcee2a633b6abd60b4857a4f833e
szport <szport@ad5c26ac-a80a-11dd-9ddb-6f86aeb5eef6>
2008-11-03 06:50:14 +0000

    1. Add support for recursive lists 2. Write dumps tests

--- trunk/dump.py
+++ trunk/dump.py
@@ -4,12 +4,137 @@ __all__ = ['dumps']
 
 null = object()
 
-class Representor(object):
+objects_cache = {}
+objects_info = {}
+NoneType = type(None)
+
+class Cacher(object):
 
     def visit(self, o):
-        name = 'visit_' + o.__class__.__name__
-        method = getattr(self, name, self.visit_object)
-        return method(o)
+        if type(o) in (NoneType, int, str, bool, float):
+            return
+        
+
+        oId = id(o)
+        global objects_cache, objects_info
+        if oId in objects_cache:
+            info = objects_info[oId]
+            if info == 0:
+                objects_info[oId] = 1
+        else:
+            objects_cache[oId] = o
+            objects_info[oId] = 0
+            name = 'visit_' + o.__class__.__name__
+            method = getattr(self, name, self.visit_object)
+            method(o)
+    #
+    def visit_list(self, o):
+        for item in o:
+            self.visit(item)
+    #
+    def visit_tuple(self, o):
+        for item in o:
+            self.visit(item)
+    #
+    def visit_object(self, o):
+        return
+    #
+    def visit_type(self, o):
+        return
+    #
+    def visit_dict(self, o):
+        for key,item in o.items():
+            self.visit(key)
+            self.visit(item)
+    
+    def visit_object(self, o):
+        if  hasattr(o, '__reduce__'):
+            state = o.__reduce__()
+            return self.with_reduce(state)
+        else:
+            newname = o.__class__.__name__
+            
+            newargs = None
+            if hasattr(o, '__getnewargs__'):
+                newargs = o.__getnewargs__()
+            
+            state = None
+            if hasattr(o, '__getstate__'):
+                state = o.__getstate__()
+            else:
+                if hasattr(o, '__dict__'):
+                    state = o.__dict__
+                else:
+                    state = {}
+                    for name in o.__slots__:
+                        value = getattr(o, name, null)
+                        if value is not null:
+                            state[name] = value
+            return self.with_not_reduce((newname, newargs, state))
+    #
+    def with_reduce(self, state):
+        n = len(state)
+        if n > 1 and state[1]:
+            for item in state[1]:
+                self.visit(item)
+        if n > 2 and state[2]:
+            if isinstance(state[2], dict):
+                for k, v in state[2].items():
+                    self.visit(v)
+            else:
+                self.visit(state)
+        if n > 3 and state[3]:
+            for v in state[3]:
+                self.visit(v)
+        if n > 4 and state[4]:
+            for k, v in state[4].items():
+                self.visit(k)
+                self.visit(v)
+    #
+    def with_not_reduce(self, state):
+        n = len(state)
+        if n > 1 and state[1]:
+            for item in state[1]:
+                self.visit(item)
+        if n > 2 and state[2]:
+            self.visit(state[2])
+
+class Representor(object):
+
+    def __init__(self, fast=True):
+        self.fast = fast
+        self.assigns = []
+        self.post_assigns = []
+        self.reprs = {}
+
+    def visit(self, o):
+        if self.fast:
+            name = 'visit_' + o.__class__.__name__
+            method = getattr(self, name, self.visit_object)
+            return method(o)
+        else:
+            oId = id(o)
+            if oId in objects_cache:
+                n = self.reprs.get(oId, None)
+                if n is None:
+                    name = 'visit_' + o.__class__.__name__
+                    method = getattr(self, name, self.visit_object)
+
+                    n = len(self.assigns)
+                    self.reprs[oId] = n
+                    varName = '_p__' + str(n)
+
+                    oRepr = method(o)
+
+                    self.assigns.append(varName + "=" + oRepr)
+                    return varName
+                else:
+                    varName = '_p__' + str(n)
+                    return varName
+            else:
+                name = 'visit_' + o.__class__.__name__
+                method = getattr(self, name, self.visit_object)
+                return method(o)
     #
     def visit_NoneType(self, o):
         return 'None'
@@ -27,7 +152,29 @@ class Representor(object):
         return repr(o)
     #
     def visit_list(self, o):
-        return '[' + ','.join(self.visit(item) for item in o) + ']'
+        if self.fast:
+            return '[' + ','.join(self.visit(item) for item in o) + ']'
+        else:
+            oId = id(o)
+            if oId in objects_cache:
+                n0 = self.reprs[id(o)]
+                lst = []
+                lst_cache = {}
+                for i,item in enumerate(o):
+                    itemId = id(item)
+                    if itemId in objects_cache:
+                        lst.append(None)
+                        lst_cache[i] = itemId
+                    else:
+                        lst.append(item)
+                oRepr = '[' + ','.join(self.visit(item) for item in lst) + ']'
+                if lst_cache:
+                    for i, itemId in lst_cache.items():
+                        n = self.reprs[itemId]
+                        self.post_assigns.append("_p__" + str(n0) + "[" + str(i) + "]=" +  "_p__" + str(n))
+                return oRepr
+            else:
+                return '[' + ','.join(self.visit(item) for item in o) + ']'
     #
     def visit_tuple(self, o):
         return '(' + ','.join(self.visit(item) for item in o) + ')'
@@ -39,7 +186,31 @@ class Representor(object):
         return o.__name__
     #
     def visit_dict(self, o):
-        return '{' + ','.join(self.visit(key) + ':' + self.visit(item) for key,item in o.items()) + '}'
+        if self.fast:
+            return '{' + ','.join(self.visit(key) + ':' + self.visit(item) for key,item in o.items()) + '}'
+        else:
+            oId = id(o)
+            if oId in objects_cache:
+                n0 = self.reprs[id(o)]
+                _dict = {}
+                dict_cache = []
+                for i,item in o.items():
+                    itemId = id(item)
+                    if itemId in objects_cache:
+                        #_dict[i] = None
+                        key = self.visit
+                        dict_cache.append((i, itemId))
+                    else:
+                        _dict[i] = item
+                oRepr = '{' + ','.join(self.visit(key) + ':' + self.visit(item) for key,item in _dict.items()) + '}'
+                if dict_cache:
+                    for i, itemId in dict_cache:
+                        key = self.visit(i)
+                        n = self.reprs[itemId]
+                        self.post_assigns.append("_p__" + str(n0) + "[" + key + "]=" +  "_p__" + str(n))
+                return oRepr
+            else:
+                return '{' + ','.join(self.visit(key) + ':' + self.visit(item) for key,item in o.items()) + '}'
     
     def visit_object(self, o):
         if  hasattr(o, '__reduce__'):
@@ -50,14 +221,23 @@ class Representor(object):
             cls = o.__class__
             if factoryType.__module__ == 'builtins':
                 if isinstance(factory, type):
-                    name = cls.__module__ + '.' + cls.__name__
+                    if cls.__module__ == '__main__':
+                        name = cls.__name__
+                    else:
+                        name = cls.__module__ + '.' + cls.__name__
                 else:
                     name = factory.__name__
             else:
                 if isinstance(factory, type):
-                    name = factoryType.__module__ + '.' + factory.__name__
+                    if factoryType.__module__ == '__main__':
+                        name = factory.__name__
+                    else:
+                        name = factoryType.__module__ + '.' + factory.__name__
                 else:
-                    name = factory.__module__ + '.' + factory.__name__
+                    if factory.__module__ == '__main__':
+                        name = factory.__name__
+                    else:
+                        name = factory.__module__ + '.' + factory.__name__
             return self.with_reduce((name,) + state[1:])
         else:
             newname = o.__class__.__name__
@@ -115,6 +295,21 @@ class Representor(object):
         ret += ')'
         return ret
 
-def dumps(o):
-    representor = Representor()
-    return representor.visit(o)
+def dumps(o, fast=True):
+    if not fast:
+        cacher = Cacher()
+        cacher.visit(o)
+        global objects_info, objects_cache
+        objects_info = {oId:n for oId,n in objects_info.items() if n > 0}
+        objects_cache = {oId:o for oId,o in objects_cache.items() if oId in objects_info}
+    representor = Representor(fast)
+    text = representor.visit(o)
+    if representor.assigns:
+        assigns = "\n".join(representor.assigns)
+    else:
+        assigns = ""
+    if representor.post_assigns:
+        post_assigns = "\n".join(representor.post_assigns)
+    else:
+        post_assigns = ""
+    return "\n".join(s for s in [assigns,post_assigns,text] if s)
