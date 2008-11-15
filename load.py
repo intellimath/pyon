02013be4fa50c94df1874526a66b4eae67ec7753
szport <szport@ad5c26ac-a80a-11dd-9ddb-6f86aeb5eef6>
2008-11-15 20:05:13 +0000

    1. Add pretty PyON representation facility. 2. Add more tests.

--- load.py
+++ load.py
@@ -1,8 +1,7 @@
 #
 import sys
 import ast
-from pyon.resolvers import defaultResolver, customResolver
-import time
+from pyon.resolvers import defaultResolver, customResolver, safeNameResolver
 
 __all__ = ['loads']
 
@@ -22,20 +21,6 @@ def _reconstructor(cls, base, state):
             base.__init__(obj, state)
     return obj
 
-def IF(self, condTree, trueTree, falseTree = None, **kwargs):
-    if self.visit(condTree):
-        return self.visit(trueTree)
-    elif falseTree is not None:
-        return self.visit(falseTree)
-
-def CASE(self, valueTree, casesTree, defaultTree = None):
-    value = self.visit(valueTree)
-    for keyTree, tree in casesTree.items():
-        if self.visit(keyTree) == value:
-            return self.visit(tree)
-    if default is not None:
-        return self.visit(defaultTree)
-
 method_cache = {}
 def cache_method(name):
     def func(m, name=name):
@@ -49,206 +34,205 @@ class NodeTransformer(object):
         self.cache = {
             'True':True, 'False':False, 
             'object':object, 'type': type, 
-            'int':int, 'string':str, 'bool':bool, 'float':float,
-            '_reconstructor':_reconstructor, 'None':None, 'IF':IF, 'CASE':CASE}
+            'int':int, 'string':str, 'bool':bool, 'float':float, 'bytes':bytes,
+            '_reconstructor':_reconstructor, 'None':None}
         self.nameResolver = nameResolver
+        self.assigns = {}
         self.post_actions = []
-    #
-    def visit(self, state):
-        name = state.__class__.__name__
-        method = method_cache[name]
-        return method(self, state)
-    #
-    @cache_method('Module')
-    def visit_Module(self, node):
-        return [x for x in (self.visit(n) for n in node.body) if x is not null]
-    #
-    @cache_method('Expr')
-    def visit_Expr(self, node):
-        return self.visit(node.value)
-    #
-    @cache_method('Attribute')
-    def visit_Attribute(self, node):
-        return getattr(self.visit(node.value), node.attr)
-    #
-    @cache_method('NoneType')
-    def visit_NoneType(self, node):
-        return None
-    #
-    @cache_method('Compare')
-    def visit_Compare(self, node):
-        left = self.visit(node.left)
-        result = True
-        for op, node_right in zip(node.ops, node.comparators):
-            opName = op.__class__.__name__
-            right = self.visit(node_right)
-            #print(opName)
-            if opName == 'Gt':
-                result = result and left > right
-            elif opName == 'Lt':
-                result = result and left < right
-            elif opName == 'LtE':
-                result = result and left <= right
-            elif opName == 'GtE':
-                result = result and left >= right
-            elif opName == 'Eq':
-                result = result and left == right
-            elif opName == 'NotEq':
-                result = result and left != right
-            else:
-                raise PyonException('Unexpected operation ' + opName)
-            left = right
-        return result
-    #
-    @cache_method('Assign')
-    def visit_Assign(self, node):
-        name = node.targets[0].id
-        value = self.visit(node.value)
-        self.cache[name] = value
-        return null
-    #
-    @cache_method('Tuple')
-    def visit_Tuple(self, node):
-        return tuple(self.visit(el) for el in node.elts)
-    #
-    @cache_method('List')
-    def visit_List(self, node):
-        lst = []
-        for i, el in enumerate(node.elts):
-            if type(el) == ast.Name:
-                try:
-                    lst.append(self.visit_Name(el))
-                except PyonResolveError:
-                    def setitem(lst=lst, i=i, name=el):
-                        lst[i] = self.visit_Name(name)
-                    lst.append(None)
-                    self.post_actions.append(setitem)
-            else:
-                lst.append(self.visit(el))
-        return lst
-        #return [self.visit(el) for el in node.elts]
-    #
-    @cache_method('Subscript')
-    def visit_Subscript(self, node):
-       item = self.visit(node.value)
-       key = self.visit(node.slice.value)
-       return item[key]
-    #    
-    @cache_method('Dict')
-    def visit_Dict(self, node):
-        _dict = {}
-        for key,value in zip(node.keys, node.values):
-            _key = self.visit(key)
-            if type(value) == ast.Name:
-                try:
-                    item = self.visit_Name(value)
-                    _dict[_key] = item
-                except PyonResolveError:
-                    def setitem(map=_dict, key=_key, name=value):
-                        map[key] = self.visit_Name(name)
-                    self.post_actions.append(setitem)
-            else:
-                _dict[_key] = self.visit(value)
-        return _dict    
-        #return dict((self.visit(key),self.visit(value)) for key,value in zip(node.keys, node.values))
+
+def visit(self, state):
+    method = method_cache.get(state.__class__.__name__, None)
+    if method is None:
+        raise PyonError('Non supporting AST node ' + state.__class__.__name__)
         
-    def visit_pyon(self, callee, *args, **kwargs):
-        if callee == IF:
-            return IF(self, *args, **kwargs)
-        elif callee == CASE:
-            return CASE(self, *args, **kwargs)
-        
-    @cache_method('Call')
-    def visit_Call(self, node):
-        callee = self.visit(node.func)
-        
-        if callee in (IF,CASE):
-           args = ()
-           kwargs = {}
-           if node.args:
-               args = tuple(arg for arg in node.args)
-           if node.keywords:
-               kwargs = dict((keyword.arg,keyword.value) for keyword in node.keywords)
-           return self.visit_pyon(callee, *args, **kwargs)
-       
-        if node.args:
-            args = tuple(self.visit(arg) for arg in node.args)
+    return method(self, state)
+#
+@cache_method('Module')
+def visit_Module(self, node):
+    return [x for x in (visit(self, n) for n in node.body) if x is not null]
+#
+@cache_method('Expr')
+def visit_Expr(self, node):
+    return visit(self, node.value)
+#
+@cache_method('Attribute')
+def visit_Attribute(self, node):
+    return getattr(visit(self, node.value), node.attr)
+#
+@cache_method('NoneType')
+def visit_NoneType(self, node):
+    return None
+#
+#@cache_method('Compare')
+#def visit_Compare(self, node):
+#    left = visit(self, node.left)
+#    result = True
+#    for op, node_right in zip(node.ops, node.comparators):
+#        opName = op.__class__.__name__
+#        right = visit(self, node_right)
+#        #print(opName)
+#        if opName == 'Gt':
+#            result = result and left > right
+#        elif opName == 'Lt':
+#            result = result and left < right
+#        elif opName == 'LtE':
+#            result = result and left <= right
+#        elif opName == 'GtE':
+#            result = result and left >= right
+#        elif opName == 'Eq':
+#            result = result and left == right
+#        elif opName == 'NotEq':
+#            result = result and left != right
+#        else:
+#            raise PyonException('Unexpected operation ' + opName)
+#        left = right
+#    return result
+#
+@cache_method('Assign')
+def visit_Assign(self, node):
+    name = node.targets[0].id
+    value = visit(self, node.value)
+    self.cache[name] = value
+    self.assigns[name] = value
+    return null
+#
+@cache_method('Tuple')
+def visit_Tuple(self, node):
+    return tuple(visit(self, el) for el in node.elts)
+#
+@cache_method('List')
+def visit_List(self, node):
+    lst = []
+    for i, el in enumerate(node.elts):
+        if el.__class__ is ast.Name:
+            try:
+                lst.append(visit_Name(self, el))
+            except PyonResolveError:
+                def setitem(lst=lst, i=i, name=el):
+                    lst[i] = visit_Name(self, name)
+                lst.append(None)
+                self.post_actions.append(setitem)
         else:
-            args = ()
-        
-        instance = callee(*args)
-        
-        if node.keywords:
-            setstate = getattr(instance, '__setstate__', None)
-            if setstate:
-                state = dict((keyword.arg, self.visit(keyword.value)) for keyword in node.keywords)
-                #state = state.get('__pyon_state__', state)
-                setstate(state)
-            else:
-                for keyword in node.keywords:
-                    valueTree = keyword.value
-                    if type(valueTree) == ast.Name:
-                        try:
-                            setattr(instance, keyword.arg, self.visit_Name(valueTree))
-                        except PyonResolveError:
-                            def _setattr(instance=instance, attr=keyword.arg, nameEl=valueTree):
-                                setattr(instance, attr, self.visit_Name(nameEl))
-                            self.post_actions.append(_setattr)
-                    else:
-                        setattr(instance, keyword.arg, self.visit(valueTree))
-                    
-                            
+            lst.append(visit(self, el))
+    return lst
+#
+@cache_method('Subscript')
+def visit_Subscript(self, node):
+   item = visit(self, node.value)
+   key = visit(self, node.slice.value)
+   return item[key]
+#    
+@cache_method('Dict')
+def visit_Dict(self, node):
+    _dict = {}
+    for key, value in zip(node.keys, node.values):
+        _key = visit(self, key)
+        if value.__class__ is ast.Name:
+            try:
+                item = visit_Name(self, value)
+                _dict[_key] = item
+            except PyonResolveError:
+                def setitem(map=_dict, key=_key, name=value):
+                    map[key] = visit_Name(self, name)
+                self.post_actions.append(setitem)
+        else:
+            _dict[_key] = visit(self, value)
+    return _dict    
+    
+def visit_pyon(self, callee, *args, **kwargs):
+    if callee == IF:
+        return IF(self, *args, **kwargs)
+    elif callee == CASE:
+        return CASE(self, *args, **kwargs)
+    
+@cache_method('Call')
+def visit_Call(self, node):
+    callee = visit(self, node.func)
 
-        if node.starargs:
-            starargs = self.visit(node.starargs)
-            for arg in starargs:
-                instance.append(arg)
-
-        if node.kwargs:
-            starkwargs = self.visit(node.kwargs)
-            for key,arg in starkwargs.items():
-                instance[key] = arg
-        
-        return instance
-    #
-    @cache_method('Num')
-    def visit_Num(self, node):
-        return node.n
-    #
-    @cache_method('Name')
-    def visit_Name(self, node):
-        try:
-            return self.cache[node.id]
-        except:
-            pass
-
-        try:
-            name = node.id
-            ob = self.nameResolver(name)
-            self.cache[name] = ob
-            return ob
-        except:
-            raise PyonResolveError("Can't resolve name " + name)
-    #
-    @cache_method('Str')
-    def visit_Str(self, node):
-        return node.s
-
-def loads(source, __resolver__=None, __modules__ = True, **kwargs):    
-        
-    if __resolver__ is None:
-        nameResolver = defaultResolver(modules = __modules__, **kwargs)
-    elif type(__resolver__) == dict:
-        nameResolver = dictResolver(__resolver__, modules = __modules__, **kwargs)
+    if node.args:
+        args = tuple(visit(self, arg) for arg in node.args)
     else:
-        nameResolver = customResolver(__resolver__, modules = __modules__, **kwargs)
-        
+        args = ()
+    
+    instance = callee(*args)
+    
+    if node.keywords:
+        setstate = getattr(instance, '__setstate__', None)
+        if setstate:
+            state = dict((keyword.arg, visit(self, keyword.value)) for keyword in node.keywords)
+            #state = state.get('__pyon_state__', state)
+            setstate(state)
+        else:
+            for keyword in node.keywords:
+                valueTree = keyword.value
+                if valueTree.__class__ is ast.Name:
+                    try:
+                        setattr(instance, keyword.arg, visit_Name(self, valueTree))
+                    except PyonResolveError:
+                        def _setattr(instance=instance, attr=keyword.arg, nameEl=valueTree):
+                            setattr(instance, attr, visit_Name(self, nameEl))
+                        self.post_actions.append(_setattr)
+                else:
+                    setattr(instance, keyword.arg, visit(self, valueTree))
+                
+                        
+
+    if node.starargs:
+        starargs = visit(self, node.starargs)
+        for arg in starargs:
+            instance.append(arg)
+
+    if node.kwargs:
+        starkwargs = visit(self, node.kwargs)
+        for key,arg in starkwargs.items():
+            instance[key] = arg
+    
+    return instance
+#
+@cache_method('Num')
+def visit_Num(self, node):
+    return node.n
+#
+@cache_method('Name')
+def visit_Name(self, node):
+    try:
+        return self.cache[node.id]
+    except:
+        pass
+
+    try:
+        name = node.id
+        ob = self.nameResolver(name)
+        self.cache[name] = ob
+        return ob
+    except:
+        raise PyonResolveError("Can't resolve name " + name)
+#
+@cache_method('Str')
+def visit_Str(self, node):
+    return node.s
+#
+@cache_method('Bytes')
+def visit_Bytes(self, node):
+    return node.s
+
+def loads(source, resolver=None, use_modules = False, given={}, safe=False):    
+
+    if safe:
+        nameResolver = safeNameResolver
+    elif resolver is None:
+        nameResolver = defaultResolver(use_modules, given)
+    elif type(resolver) == dict:
+        nameResolver = dictResolver(resolver, use_modules, given)
+    elif use_modules or given:
+        nameResolver = customResolver(resolver, use_modules, given)
+
     transformer = NodeTransformer(nameResolver)
     if issubclass(type(source), str):
         tree = ast.parse(source)
-        ob = transformer.visit(tree)[-1]
+        ob = visit(transformer, tree)[-1]
     elif issubclass(type(source), ast.AST):
-        ob = transformer.visit(source)[-1]
+        ob = visit(transformer, source)[-1]
     else:
         PyonError('Source must be a string or AST', source)
     if transformer.post_actions:
--- resolvers.py
+++ resolvers.py
@@ -1,52 +1,30 @@
 #
 import sys
 
-def defaultResolver(modules = True, **kwargs):
-    globalsDict = dict(sys._getframe(2).f_globals)
-    localsDict = dict(sys._getframe(2).f_locals)
-    if kwargs:
-        localsDict.update(kwargs)
-    return dictResolver(globalsDict, modules, **localsDict)
+def defaultResolver(use_modules = True, given={}):
+    _dict = dict(sys._getframe(2).f_globals)
+    _dict.update(sys._getframe(2).f_locals)
+    if given:
+        _dict.update(given)
+    return dictResolver(_dict, use_modules)
     
-def dictResolver(globalsDict, modules=True, **kwargs):
-    localsDict = dict(kwargs)
-    if localsDict:
-        if modules:
+def dictResolver(_dict, use_modules=True):
+        if use_modules:
             def resolver(name):
                 try:
-                    return globalsDict[name]
-                except: pass
-                try:
-                    return localsDict[name]
+                    return _dict[name]
                 except: pass
                 return sys.modules[name]
             return resolver
         else:
             def resolver(name):
-                try:
-                    return globalsDict[name]
-                except: pass
-                return localsDict[name]
-            return resolver
-    else:
-        if modules:
-            def resolver(name):
-                try:
-                    return globalsDict[name]
-                except: pass
-                return sys.modules[name]
-            return resolver
-        else:
-            def resolver(name):
-                try:
-                    return globalsDict[name]
-                except: pass
+                return _dict[name]
             return resolver
 
-def customResolver(nameResolver, modules=True, **kwargs):
-    localsDict = dict(kwargs)
+def customResolver(nameResolver, use_modules=True, given={}):
+    localsDict = dict(given)
     if localsDict:
-        if modules:
+        if use_modules:
             def resolver(name):
                 try:
                     return nameResolver(name)
@@ -66,12 +44,56 @@ def customResolver(nameResolver, modules=True, **kwargs):
                 except: pass
             return resolver
     else:
-        if modules:
+        if use_modules:
             def resolver(name):
                 return nameResolver(name)
                 return sys.modules[name]
             return resolver
         else:
-            def resolver(name):
-                return nameResolver(name)
-            return resolver
+            return nameResolver
+
+safe_cache = {}
+def safeNameResolver(name):
+    C = safe_cache.get(name, None)
+    if C is None:
+        C = safe_cache[name] = type(name, (_Element_,), {'__module__' : '__cache__'})
+    return C
+
+class _Element_(object):
+    #
+    def __init__(self, *args):
+        self._args__ = args
+        self._sequence__ = []
+        self._mapping__ = {}
+    #
+    @property
+    def tag(self):
+        return self.__class__.__name__
+    #
+    @property
+    def args(self):
+        return self.__args
+    #
+    @property
+    def sequence(self):
+        return self.__sequence
+    #
+    @property
+    def mapping(self):
+        return self.__mapping
+    #
+    def append(self, item):
+        self.sequence.append(item)
+    #
+    def __setitem__(self, key, item):
+        self.mapping[key] = item
+    #
+    def __reduce__(self):
+        _dict = dict(self.__dict__)
+        args = _dict.pop('_args__', None)
+        sequence = _dict.pop('_sequence__', None)
+        mapping = _dict.pop('_mapping__', None)
+        return safeNameResolver(self.__class__.__name__), args, _dict, sequence, mapping
+    #
+    def __setstate__(self, kwargs):
+        self.__dict__.update(kwargs)
